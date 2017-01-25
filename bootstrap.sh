b617a0436f00ebc1c3fc4db43072254124d51c15
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-01-25 14:25:39 +0100

    Fix bootstrap if

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ echo node version: `node --version`
 git clone $GIT_URL app
 cd app
 
-if [ "$YARN_INSTALL" = "1"]; then
+if [ "$YARN_INSTALL" = "1" ]; then
   yarn install --production --silent
 else
   npm install --production --silent
